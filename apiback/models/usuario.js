f3ca147b1e642dca41f691e3d82648edc17217d3
Joaquín Tommasi <joaco1205@gmail.com>
2025-02-01 12:18:40 -0300

    Creé modelo para usuario.

---
+++ apiback/models/usuario.js
@@ -0,0 +1,54 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class Usuario extends Model {
+        static associate(models) {
+            //Nada.
+        }
+    }
+
+    Usuario.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        nombre: {
+            type: DataTypes.STRING(32),
+            allowNull: false,
+            unique: true,
+        },
+        estaActivo: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            field: 'esta_activo',
+            defaultValue: true,
+        },
+        contrasenia: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+        correo: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: true,
+        },
+        esAdmin:{
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            field: 'es_admin',
+            defaultValue: false,
+        },
+    }, {
+        sequelize,
+        modelName: 'Usuario',
+        tableName: 'usuarios',
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    });
+
+    return Usuario;
+};
