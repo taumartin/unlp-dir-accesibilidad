8557c76e3f115c4710d413f9633eb488b26d05d8
Martin E. Tau <taumartin@gmail.com>
2025-02-07 22:40:08 -0300

    Minor fix.

--- apiback/routes/api/personas.js
+++ apiback/routes/api/personas.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const personaController = require('../../controllers/persona');
 
-/* Create Persona. */
+/** Create Persona. */
 router.post('/', personaController.create);
+/** List all Personas. */
 router.get('/', personaController.listAll);
+/** Get Persona by ID. */
 router.get('/:id', personaController.findById);
+/** Update Persona by ID. */
 router.post('/:id', personaController.update);
+/** Delete Persona by ID. */
 router.delete('/:id', personaController.delete);
 
 module.exports = router;
