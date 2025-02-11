5517a7378d402f91e03305a63a04be6dbcd433a6
Martin E. Tau <taumartin@gmail.com>
2025-02-11 18:15:06 -0300

    Contenido de pag. error 404

--- appfront/src/app/pages/error-not-found/error-not-found.component.spec.ts
+++ appfront/src/app/pages/error-not-found/error-not-found.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ErrorNotFoundComponent } from './error-not-found.component';
+import {ErrorNotFoundComponent} from './error-not-found.component';
 
 describe('ErrorNotFoundComponent', () => {
   let component: ErrorNotFoundComponent;
@@ -9,8 +9,7 @@ describe('ErrorNotFoundComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ErrorNotFoundComponent]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ErrorNotFoundComponent);
     component = fixture.componentInstance;
--- appfront/src/app/pages/error-not-found/error-not-found.component.ts
+++ appfront/src/app/pages/error-not-found/error-not-found.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-error-not-found',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './error-not-found.component.html',
   styleUrl: './error-not-found.component.scss'
 })
 export class ErrorNotFoundComponent {
-
 }
