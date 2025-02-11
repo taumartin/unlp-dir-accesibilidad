11128448a33cb630f4191ec75a8eb42f72ded320
Martin E. Tau <taumartin@gmail.com>
2025-02-11 17:40:32 -0300

    Agregaro funcion scroll-top

--- appfront/src/app/app.component.ts
+++ appfront/src/app/app.component.ts
@@ -1,16 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {SidebarComponent} from './components/sidebar/sidebar.component';
 import {TopbarComponent} from './components/topbar/topbar.component';
 import {FooterComponent} from './components/footer/footer.component';
+import {faAngleUp,} from '@fortawesome/free-solid-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {debounceTime, fromEvent, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, TopbarComponent, FooterComponent],
+  imports: [RouterOutlet, SidebarComponent, TopbarComponent, FooterComponent, FaIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  public iconUp = faAngleUp;
+  private scrollSubscription: Subscription | null = null;
+  public showScrollTop: boolean = false;
+
   title = 'appfront';
 
   public test_result: string | null = null;
@@ -28,9 +35,29 @@ export class AppComponent implements OnInit {
     }
   }
 
+  private onDocumentScroll(scrollDistance: number): void {
+    this.showScrollTop = (scrollDistance > 100);
+  }
+
+  public scrollToTop(): void {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
   public ngOnInit(): void {
-    this.testApi().then((result) => {
+    this.scrollSubscription = fromEvent(document, 'scroll')
+      .pipe(debounceTime(100))
+      .subscribe(() => {
+        this.onDocumentScroll(window.scrollY);
+      });
+    this.testApi().then((result) => { // FIXME: quitar..
       this.test_result = JSON.stringify(result);
     });
+  }
+
+  public ngOnDestroy(): void {
+    if (this.scrollSubscription) {
+      this.scrollSubscription.unsubscribe();
+      this.scrollSubscription = null;
+    }
   }
 }
