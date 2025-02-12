cb992c6bb8af5dd56b066394fb1f7de05e3f9ba5
Martin E. Tau <taumartin@gmail.com>
2025-02-12 00:10:02 -0300

    Fix creacion de persona.

--- apiback/controllers/persona.js
+++ apiback/controllers/persona.js
@@ -2,13 +2,13 @@ const {Persona} = require('../models');
 const personaRepository = require("../repositories/persona").getInstance();
 
 module.exports.create = function (req, res) {
-    return personaRepository.create({
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        dni: req.body.dni,
-        telefono: req.body.telefono,
-        email: req.body.email,
-    })
+    return personaRepository.create(
+        req.body.nombre,
+        req.body.apellido,
+        req.body.dni,
+        req.body.telefono,
+        req.body.email
+    )
         .then(persona => res.status(200).send(persona))
         .catch(error => res.status(400).send(error));
 };
