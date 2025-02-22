01a7b46f21f675684a0b133c88a30bc826eb2949
Martin E. Tau <taumartin@gmail.com>
2025-02-22 03:38:46 -0300

    Creado layout de formulario forgot password.

--- appfront/src/app/pages/auth/forgot-password/forgot-password.component.ts
+++ appfront/src/app/pages/auth/forgot-password/forgot-password.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
