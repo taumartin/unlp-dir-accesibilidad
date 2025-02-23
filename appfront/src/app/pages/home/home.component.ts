d2d72364aa02f92dd259f7cbeddcc3dce582a4c6
Martin E. Tau <taumartin@gmail.com>
2025-02-23 17:07:50 -0300

    Agregado mensajes de bienvenida a pantalla home.

--- appfront/src/app/pages/home/home.component.ts
+++ appfront/src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {PageHeadingComponent} from "../../components/page-heading/page-heading.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [PageHeadingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
