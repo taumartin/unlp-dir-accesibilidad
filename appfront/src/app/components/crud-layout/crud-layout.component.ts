b62a97d3e9063e4733daf754d6078db322c01856
Martin E. Tau <taumartin@gmail.com>
2025-02-24 15:37:55 -0300

    Agregado edición de persona en CRUD layout front.

--- appfront/src/app/components/crud-layout/crud-layout.component.ts
+++ appfront/src/app/components/crud-layout/crud-layout.component.ts
@@ -1,7 +1,7 @@
-import {Component, EventEmitter, Input, OnInit, Output, TemplateRef} from '@angular/core';
-import {DataTablesModule} from 'angular-datatables';
+import {Component, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild} from '@angular/core';
+import {DataTableDirective, DataTablesModule} from 'angular-datatables';
 import {PageHeadingComponent} from '../page-heading/page-heading.component';
-import {Config, ConfigColumns} from 'datatables.net';
+import {Api, Config, ConfigColumns} from 'datatables.net';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {NgTemplateOutlet} from '@angular/common';
 import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
@@ -10,6 +10,13 @@ import {ApiPageRequest} from '../../services/network/api/api-page-request';
 import {Observable} from 'rxjs';
 import {ApiResponsePage} from '../../services/network/api/api-response-page';
 import {DatatablesService} from '../../services/data/datatables/datatables.service';
+import {ToastService} from '../../services/ui/toast/toast.service';
+
+export interface CrudLayoutEntityModalOpenEvent<T> {
+  entity?: T;
+  rowIndex?: number;
+  modal: NgbModalRef;
+}
 
 @Component({
   selector: 'app-crud-layout',
@@ -25,6 +32,12 @@ import {DatatablesService} from '../../services/data/datatables/datatables.servi
 export class CrudLayoutComponent<T> implements OnInit {
   protected iconCreate = faPlus;
   protected dtOptions: Config = {};
+  protected isEditing = false;
+
+  @ViewChild('entityModalTemplate')
+  protected entityModalTemplate!: TemplateRef<any>;
+  @ViewChild(DataTableDirective, {static: false})
+  protected dtElement!: DataTableDirective;
 
   @Input()
   public labels: { [key: string]: string; } = {};
@@ -33,56 +46,79 @@ export class CrudLayoutComponent<T> implements OnInit {
   @Input()
   public dtSource!: (pagReq: ApiPageRequest) => Observable<ApiResponsePage<T>>;
   @Input()
-  public createModalTemplate?: TemplateRef<any>;
+  public entityModalBodyTemplate?: TemplateRef<any>;
   @Input()
   public formInvalid: boolean = true;
   @Input()
   public isProcessing: boolean = false;
 
   @Output()
-  public createEntity = new EventEmitter<NgbModalRef>();
+  public entityModalOpen = new EventEmitter<CrudLayoutEntityModalOpenEvent<T>>();
   @Output()
-  public entitySaved = new EventEmitter<NgbModalRef>();
+  public entityModalDismiss = new EventEmitter<void>();
+  @Output()
+  public entityModalSave = new EventEmitter<NgbModalRef>();
 
   constructor(
     private readonly modalService: NgbModal,
     private readonly datatablesService: DatatablesService,
+    private readonly toastService: ToastService,
   ) {
   }
 
   private onRowClick(dataRow: T, rowIndex: number): void {
-    // TODO: lanzar evento al host para popular el form, abrir modal...
-    console.log("rowcallback", dataRow, rowIndex);
+    this.isEditing = true;
+    this.openEntityModal(dataRow, rowIndex);
+  }
+
+  private refreshTable(): void {
+    if (this.dtElement) {
+      this.dtElement.dtInstance.then((dtInstance: Api) => {
+        dtInstance.ajax.reload(undefined, false);
+      });
+    }
   }
 
   public ngOnInit(): void {
     this.dtOptions = this.datatablesService.getOptionsServerSide(this.dtColumns, this.dtSource,
-      (data: T, index: number): void => this.onRowClick(data, index));
+      this.entityModalBodyTemplate ? (data: T, index: number): void => this.onRowClick(data, index) : undefined);
   }
 
-  private openModal(modalTemplate: TemplateRef<any>, openedByButton: HTMLButtonElement): NgbModalRef {
-    const modalRef = this.modalService.open(modalTemplate, {
+  private openEntityModal(entity?: T, rowIndex?: number): NgbModalRef {
+    const modal = this.modalService.open(this.entityModalTemplate, {
       backdrop: 'static',
       keyboard: true,
       centered: true,
       size: "xl",
       modalDialogClass: 'modal-dialog-centered modal-dialog-scrollable',
     });
-    modalRef.result.finally(() => {
-      openedByButton.focus();
-    });
-    return modalRef;
+    this.entityModalOpen.emit({entity, modal, rowIndex});
+    modal.result
+      .then(() => {
+        this.refreshTable();
+      })
+      .catch(dismissReason => {
+        if (![1, 'CANCELLED', 'CLOSED'].includes(dismissReason)) {
+          this.toastService.showStandardToast({body: 'Sucedió un error y en consecuencia se cerró la ventana modal.'});
+        }
+      });
+    return modal;
   }
 
   protected dismissModal(modal: NgbModalRef, reason: string): void {
     modal.dismiss(reason);
+    this.entityModalDismiss.emit();
+  }
+
+  protected openEntityModalForCreation(btn: HTMLButtonElement): void {
+    this.isEditing = false;
+    const modal = this.openEntityModal();
+    modal.result.finally(() => {
+      btn.focus();
+    });
   }
 
   protected onSaveEntity(modal: NgbModalRef): void {
-    this.entitySaved.emit(modal);
-  }
-
-  protected openCreationModal(modalTemplate: TemplateRef<any>, btn: HTMLButtonElement): void {
-    this.createEntity.emit(this.openModal(modalTemplate, btn));
+    this.entityModalSave.emit(modal);
   }
 }
--- appfront/src/app/pages/abms/abm-personas/abm-personas.component.ts
+++ appfront/src/app/pages/abms/abm-personas/abm-personas.component.ts
@@ -2,7 +2,10 @@ import {Component, inject} from '@angular/core';
 import {DataTablesModule} from 'angular-datatables';
 import {ConfigColumns} from 'datatables.net';
 import {PersonasService} from '../../../services/data/personas/personas.service';
-import {CrudLayoutComponent} from '../../../components/crud-layout/crud-layout.component';
+import {
+  CrudLayoutComponent,
+  CrudLayoutEntityModalOpenEvent
+} from '../../../components/crud-layout/crud-layout.component';
 import {AbstractControl, FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import {NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import {FormService} from '../../../services/ui/form/form.service';
@@ -11,6 +14,8 @@ import {Persona} from '../../../models/persona';
 import {ApiPageRequest} from '../../../services/network/api/api-page-request';
 import {Observable} from 'rxjs';
 import {ApiResponsePage} from '../../../services/network/api/api-response-page';
+import {ApiSuccessResponse} from '../../../services/network/api/api-success-response';
+import {ApiErrorResponse} from '../../../services/network/api/api-error-response';
 
 @Component({
   selector: 'app-abm-personas',
@@ -22,6 +27,7 @@ export class AbmPersonasComponent {
   protected labels = {
     title: 'ABM Personas',
     creation: 'Nueva Persona',
+    edition: 'Ver/Editar Persona',
   }
   protected readonly dtColumns: ConfigColumns[] = [
     {title: 'ID', data: 'id', name: 'id', className: 'text-start'},
@@ -33,12 +39,16 @@ export class AbmPersonasComponent {
   ];
   protected dtSource: (pagReq: ApiPageRequest) => Observable<ApiResponsePage<Persona>> = (pagReq) => this.personasService.getPersonas(pagReq);
   protected isCreatingPersona = false;
+  protected isEditingPersona = false;
+
+  private entityModal: NgbModalRef | null = null;
+  private selectedPersona: Persona | null = null;
 
   private readonly formBuilder = inject(FormBuilder);
   protected personaForm = this.formBuilder.group({
     name: ['', [Validators.required, Validators.maxLength(100)]],
     lastname: ['', [Validators.required, Validators.maxLength(100)]],
-    documentNumber: [null, [Validators.required, Validators.min(10_000_000), Validators.max(99_999_999),
+    documentNumber: [0, [Validators.required, Validators.min(10_000_000), Validators.max(99_999_999),
       Validators.pattern(/^\d{8}$/)]],
     email: ['', [Validators.required, Validators.email, Validators.maxLength(100)]],
     phone: ['', [Validators.maxLength(25)]],
@@ -79,44 +89,80 @@ export class AbmPersonasComponent {
     return this.formService.isInputValid(control);
   }
 
-  private createPersona(persona: Omit<Persona, 'id'>, modal: NgbModalRef): void {
-    this.isCreatingPersona = true;
-    this.personasService.createPersona(persona).subscribe({
-      next: result => {
-        if (result.success) {
-          this.personaForm.reset();
-          this.toastService.showSuccessToast({body: result.message});
-          modal.close();
-        } else {
-          this.formService.parseBackendValidation(this.personaForm, result);
-          this.toastService.showErrorToast({body: result.message});
-        }
-      },
-      complete: () => {
-        this.isCreatingPersona = false;
-      }
-    });
+  private closeModal(): void {
+    this.entityModal?.close();
+    this.entityModal = null;
   }
 
-  protected onSavePersona(modal: NgbModalRef) {
-    if (this.personaForm.valid && !this.isCreatingPersona) {
+  private createPersona(persona: Omit<Persona, 'id'>): void {
+    console.log("creating");
+    if (!this.isCreatingPersona) {
+      this.isCreatingPersona = true;
+      this.personasService.createPersona(persona).subscribe({
+        next: result => this.onEntitySaveEnd(result),
+        complete: () => {
+          this.isCreatingPersona = false;
+        }
+      });
+    }
+  }
+
+  private onEntitySaveEnd(result: ApiSuccessResponse | ApiErrorResponse): void {
+    if (result.success) {
+      this.personaForm.reset();
+      this.toastService.showSuccessToast({body: result.message});
+      this.closeModal();
+      this.selectedPersona = null;
+    } else {
+      this.formService.parseBackendValidation(this.personaForm, result);
+      this.toastService.showErrorToast({body: result.message});
+    }
+  }
+
+  private editPersona(persona: Omit<Persona, 'id'>): void {
+    if (!this.isEditingPersona && this.selectedPersona) {
+      this.isEditingPersona = true;
+      this.personasService.updatePersona(this.selectedPersona.id, persona).subscribe({
+        next: result => this.onEntitySaveEnd(result),
+        complete: () => {
+          this.isEditingPersona = false;
+        }
+      });
+    }
+  }
+
+  protected onSavePersona(modal: NgbModalRef | null = null) {
+    if (this.personaForm.valid) {
+      this.entityModal = this.entityModal || modal;
       this.formService.resetFormValidations(this.personaForm);
       const {name, lastname, documentNumber, email, phone} = this.personaForm.value;
-      this.createPersona({
+      const newPersona: Omit<Persona, 'id'> = {
         nombre: name!,
         apellido: lastname!,
         dni: Number(documentNumber!),
         email: email!,
         telefono: phone ?? '',
-      }, modal);
+      };
+      if (this.selectedPersona) {
+        this.editPersona(newPersona)
+      } else {
+        this.createPersona(newPersona);
+      }
     }
   }
 
-  protected onEntitySaved(modal: NgbModalRef): void {
-    this.onSavePersona(modal);
-  }
-
-  protected onCreateEntity(modal: NgbModalRef): void {
+  protected onEntityModalOpen(event: CrudLayoutEntityModalOpenEvent<Persona>): void {
     this.personaForm.reset();
+    this.entityModal = event.modal;
+    this.selectedPersona = event.entity || null;
+    if (this.selectedPersona) {
+      this.personaForm.patchValue({
+        name: this.selectedPersona.nombre,
+        lastname: this.selectedPersona.apellido,
+        documentNumber: this.selectedPersona.dni,
+        email: this.selectedPersona.email,
+        phone: this.selectedPersona.telefono,
+      });
+    }
   }
 }
