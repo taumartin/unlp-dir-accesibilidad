2b8f4904d44d86846e9a1b1e38d01cced8f3ac5d
Martin E. Tau <taumartin@gmail.com>
2025-03-11 18:37:17 -0300

    Implementado front CRUD para Materia.

--- appfront/src/app/pages/abms/abm-materias/abm-materias.component.ts
+++ appfront/src/app/pages/abms/abm-materias/abm-materias.component.ts
@@ -1,33 +1,75 @@
 import {Component} from '@angular/core';
 import {DataTablesModule} from 'angular-datatables';
-import {ConfigColumns} from 'datatables.net';
 import {MateriasService} from '../../../services/data/materias/materias.service';
 import {CrudLayoutComponent} from '../../../components/crud-layout/crud-layout.component';
-import {ApiPageRequest} from '../../../services/network/api/api-page-request';
-import {Observable} from 'rxjs';
-import {ApiResponsePage} from '../../../services/network/api/api-response-page';
 import {Materia} from '../../../models/materia';
+import {GenericAbm} from '../generic-abm';
+import {FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormService} from '../../../services/ui/form/form.service';
+import {ToastService} from '../../../services/ui/toast/toast.service';
 
 @Component({
   selector: 'app-abm-materias',
-  imports: [DataTablesModule, CrudLayoutComponent],
+  imports: [DataTablesModule, CrudLayoutComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './abm-materias.component.html',
   styleUrl: './abm-materias.component.scss'
 })
-export class AbmMateriasComponent {
-  protected labels = {
-    title: 'ABM Materias',
-  };
-  protected readonly dtColumns: ConfigColumns[] = [
-    {title: 'ID', data: 'id', name: 'id', className: 'text-start'},
-    {title: 'Nombre', data: 'nombre', name: 'nombre'},
-    {title: 'Docentes', data: 'docentes', name: 'docentes'},
-    {title: 'Contacto', data: 'contacto', name: 'contacto'},
-  ];
-  protected dtSource: (pagReq: ApiPageRequest) => Observable<ApiResponsePage<Materia>> = (pagReq) => this.materiasService.listAll(pagReq);
-
+export class AbmMateriasComponent extends GenericAbm<Materia> {
   public constructor(
-    private readonly materiasService: MateriasService,
+    materiasService: MateriasService,
+    formService: FormService,
+    toastService: ToastService,
   ) {
+    super(materiasService, formService, toastService, [
+      {title: 'ID', data: 'id', name: 'id', className: 'text-start'},
+      {title: 'Nombre', data: 'nombre', name: 'nombre'},
+      {title: 'Docentes', data: 'docentes', name: 'docentes'},
+      {title: 'Contacto', data: 'contacto', name: 'contacto'},
+    ], {
+      title: 'ABM Materias',
+      creation: 'Nueva Materia',
+      edition: 'Ver/Editar Materia',
+    }, {
+      nombre: ['', [Validators.required, Validators.maxLength(100)]],
+      docentes: ['', [Validators.maxLength(500)]],
+      contacto: ['', [Validators.maxLength(500)]],
+    });
+  }
+
+  protected get nombre() {
+    return this.getFormControlByKey('nombre');
+  }
+
+  protected get docentes() {
+    return this.getFormControlByKey('docentes');
+  }
+
+  protected get contacto() {
+    return this.getFormControlByKey('contacto');
+  }
+
+  protected getModified(formValues: Partial<Omit<Materia, "id">>): Partial<Omit<Materia, "id">> {
+    return {
+      nombre: formValues.nombre ?? undefined,
+      docentes: formValues.docentes ?? undefined,
+      contacto: formValues.contacto ?? undefined,
+    };
+  }
+
+  protected getNewModel(formValues: Partial<Omit<Materia, "id">>): Omit<Materia, "id"> {
+    const {nombre, docentes, contacto} = formValues;
+    return {
+      nombre: nombre!,
+      docentes: docentes ?? '',
+      contacto: contacto ?? '',
+    };
+  }
+
+  protected getPatchedValues(model: Materia): any {
+    return {
+      nombre: model.nombre,
+      docentes: model.docentes,
+      contacto: model.contacto,
+    };
   }
 }
